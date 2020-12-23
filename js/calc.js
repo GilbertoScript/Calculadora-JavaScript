e65fc2a66fffb07f42a07988ceb71683497037de
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2020-12-23 09:54:32 -0300

    Add file via upload

---
+++ js/calc.js
@@ -0,0 +1,25 @@
+var valor
+var resultado
+
+function botao(num) {
+	valor = document.calc.visor.value += num
+}
+
+function calcula() {
+	resultado = eval(valor)
+	document.calc.visor.value = resultado.toLocaleString('pt-br')
+}
+
+// Aceitar apenas números no Visor
+
+function onlynumber(evt) {
+   var theEvent = evt || window.event;
+   var key = theEvent.keyCode || theEvent.which;
+   key = String.fromCharCode( key );
+   //var regex = /^[0-9.,]+$/;
+   var regex = /^[0-9.]+$/;
+   if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+   }
+}
