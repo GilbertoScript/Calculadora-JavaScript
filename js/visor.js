0d98df57baee9b6a6d8022db386640efabaf1645
Gilberto Alves <56325350+GilbertoASJ@users.noreply.github.com>
2021-02-01 16:28:45 -0300

    Create js file

---
+++ js/visor.js
@@ -0,0 +1,13 @@
+// Aceitar apenas números no Visor
+
+function onlynumber(evt) {
+   var theEvent = evt || window.event;
+   var key = theEvent.keyCode || theEvent.which;
+   key = String.fromCharCode( key );
+   //var regex = /^[0-9.,]+$/;
+   var regex = /^[0-9.]+$/;
+   if( !regex.test(key) ) {
+      theEvent.returnValue = false;
+      if(theEvent.preventDefault) theEvent.preventDefault();
+   }
+}
